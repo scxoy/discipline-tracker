f9996a70701bb9870d5777179a4c4603fb47760e
scxoy <258273854+scxoy@users.noreply.github.com>
2026-02-16 21:11:05 +0100

    add reward, profit and R ratio

--- backtests/risk_calculator.py
+++ backtests/risk_calculator.py
@@ -3,6 +3,7 @@ capital = float(input("Capital total : "))
 risk_percent = float(input("Risque en % par trade : "))
 entry_price = float(input("Prix d'entrée : "))
 stop_price = float(input("Prix du stop : "))
+take_profit = float(input("Prix take profit : "))
 
 risk_amount = capital * (risk_percent / 100)
 distance = abs(entry_price - stop_price)
@@ -10,6 +11,14 @@ if distance == 0:
     print("Distance invalide.")
 else: 
     quantity = risk_amount / distance
+    reward_distance = abs(take_profit - entry_price)
+    profit = reward_distance * quantity
+    r_ratio = profit / risk_amount
+
     print(f"Montant risqué : {risk_amount}")
     print(f"Distance : {distance}")
     print(f"Quantité à prendre : {quantity}")
+    print(f"Distance reward : {reward_distance}")
+    print(f"Profit potentiel : {profit}")
+    print(f"R ratio : {r_ratio}")
+    
