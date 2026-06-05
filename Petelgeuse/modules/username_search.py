e29efc8fdc57db6f645306d4d9329d7e123b87ba
scxoy <258273854+scxoy@users.noreply.github.com>
2026-06-05 18:57:56 +0200

    Ajout interpretation status et extension Petelgeuse

--- Petelgeuse/modules/username_search.py
+++ Petelgeuse/modules/username_search.py
@@ -1,20 +1,46 @@
-def search_username(pseudo):
-    sites = [
-        "https://github.com/",
-        "https://www.reddit.com/user/",
-        "https://www.youtube.com/@",
-        "https://x.com/",
-        "https://www.tiktok.com/@",
-    ]
+import requests
 
+sites = [
+    ["Github", "https://github.com/"],
+    ["YouTube", "https://www.youtube.com/user/"],
+    ["Reddit", "https://www.reddit.com/user/"],
+    ["TikTok", "https://www.tiktok.com/@"],
+    ["Instagram", "https://www.instagram.com/"],
+    ["X", "https://x.com/"],
+    ["facebook", "https://www.facebook.com/"],
+    ["Twitch", "https://www.twitch.tv/"],
+    ["Spotify", "https://open.spotify.com/user/"],
+    ["Discord", "https://discord.com/users/"],
+    ["Steam", "https://steamcommunity.com/id/"],
+    ["Pinterest", "https://www.pinterest.com/"],
+    ["LinkedIn", "https://www.linkedin.com/in/"],
+    ["Snapchat", "https://www.snapchat.com/add/"],
+]
+
+def search_username(pseudo):
     compteur = 0
 
     for site in sites:
-        url = site + pseudo
+        nom_site = site[0]
+        url_base = site[1]
+        url = url_base + pseudo
         print(url)
+        response = requests.get(url, timeout=5)
+        status = interpret_status(response.status_code)
+        print(status)
         compteur += 1
 
     return compteur
 
 def compter_sites():
-    return 5
+    return len(sites)
+
+def interpret_status(code):
+    if code == 200:
+        return "Trouvé"
+    elif code == 403:
+        return "Refusé"
+    elif code == 404:
+        return "Non trouvé"
+    else:
+        return "Inconnu"
